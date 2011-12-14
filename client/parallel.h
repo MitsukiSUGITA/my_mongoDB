gregstuder/mongo

allow commands to be run sharded via ParallelCursor SERVER-4220

client/parallel.h
@@ -232,6 +232,9 @@ namespace mongo {
         string versionedNS;
         BSONObj cmdFilter;
 
+        CommandInfo() {}
+        CommandInfo( const string& vns, const BSONObj& filter ) : versionedNS( vns ), cmdFilter( filter ) {}
+
         bool isEmpty(){
             return versionedNS == "";
         }
@@ -309,7 +312,8 @@ namespace mongo {
         ParallelSortClusteredCursor( const set<ServerAndQuery>& servers , QueryMessage& q , const BSONObj& sortKey );
         ParallelSortClusteredCursor( const set<ServerAndQuery>& servers , const string& ns ,
                                      const Query& q , int options=0, const BSONObj& fields=BSONObj() );
-        ParallelSortClusteredCursor( const QueryMessage& qMess, const CommandInfo& cInfo );
+        ParallelSortClusteredCursor( const QueryMessage& qMess, const CommandInfo& cInfo = CommandInfo() );
+        ParallelSortClusteredCursor( const set<Shard>& servers, const QueryMessage& qMess );
         virtual ~ParallelSortClusteredCursor();
         virtual bool more();
         virtual BSONObj next();
@@ -319,8 +323,12 @@ namespace mongo {
         void startInit();
         void finishInit();
 
+        bool isCommand(){ return nsIsCmd( _qMess.nspace() ); }
+        bool isVersioned(){ return _qShards.size() == 0; }
+
         bool isSharded();
         ShardPtr getPrimary();
+        void getQueryShards( set<Shard>& shards );
         ChunkManagerPtr getChunkManager( const Shard& shard );
         DBClientCursorPtr getShardCursor( const Shard& shard );
 
@@ -334,10 +342,10 @@ namespace mongo {
 
         virtual void _explain( map< string,list<BSONObj> >& out );
 
-        bool _isCommand(){ return ! _cInfo.isEmpty(); }
         void _markStaleNS( const string& staleNS, bool& forceReload, bool& fullReload );
         void _handleStaleNS( const string& staleNS, bool forceReload, bool fullReload );
 
+        set<Shard> _qShards;
         ScopedQueryMessage _qMess;
         CommandInfo _cInfo;
         // Count round-trips req'd for namespaces and total

s/strategy_shard.cpp
@@ -90,6 +90,29 @@ namespace mongo {
             }
         }
 
+        virtual void commandOp( const string& db, const BSONObj& command, int options,
+                                const string& versionedNS, const BSONObj& filter,
+                                map<Shard,BSONObj>& results )
+        {
+
+            ScopedQueryMessage qMess( db + ".$cmd", command, BSONObj(), 0, 1, options );
+
+            ParallelSortClusteredCursor cursor( qMess, CommandInfo( versionedNS, filter ) );
+
+            // Initialize the cursor
+            cursor.init();
+
+            log() << "Cursor : " << cursor.toString() << endl;
+
+            set<Shard> shards;
+            cursor.getQueryShards( shards );
+
+            for( set<Shard>::iterator i = shards.begin(), end = shards.end(); i != end; ++i ){
+                results[ *i ] = cursor.getShardCursor( *i )->peekFirst().getOwned();
+            }
+
+        }
+
         virtual void getMore( Request& r ) {
 
             // TODO:  Handle stale config exceptions here from coll being dropped or sharded during op

MitsukiSUGITA/my_mongoDB

client/parallel.h
@@ -233,6 +233,9 @@ namespace mongo {
         string versionedNS;
         BSONObj cmdFilter;
 
+        CommandInfo() {}
+        CommandInfo( const string& vns, const BSONObj& filter ) : versionedNS( vns ), cmdFilter( filter ) {}
+
         bool isEmpty(){
             return versionedNS.size() == 0;
         }
@@ -291,6 +294,8 @@ namespace mongo {
         bool finished;
         bool completed;
 
+        bool errored;
+
         BSONObj toBSON() const;
 
         string toString() const {
@@ -310,10 +315,15 @@ namespace mongo {
      */
     class ParallelSortClusteredCursor : public ClusteredCursor {
     public:
+
+        ParallelSortClusteredCursor( const QuerySpec& qSpec, const CommandInfo& cInfo = CommandInfo() );
+        ParallelSortClusteredCursor( const set<Shard>& servers, const QuerySpec& qSpec );
+
+        // LEGACY Constructors
         ParallelSortClusteredCursor( const set<ServerAndQuery>& servers , QueryMessage& q , const BSONObj& sortKey );
         ParallelSortClusteredCursor( const set<ServerAndQuery>& servers , const string& ns ,
                                      const Query& q , int options=0, const BSONObj& fields=BSONObj() );
-        ParallelSortClusteredCursor( const QuerySpec& qSpec, const CommandInfo& cInfo );
+
         virtual ~ParallelSortClusteredCursor();
         virtual bool more();
         virtual BSONObj next();
@@ -323,8 +333,12 @@ namespace mongo {
         void startInit();
         void finishInit();
 
+        bool isCommand(){ return NamespaceString( _qSpec.ns() ).isCommand(); }
+        bool isVersioned(){ return _qShards.size() == 0; }
+
         bool isSharded();
         ShardPtr getPrimary();
+        void getQueryShards( set<Shard>& shards );
         ChunkManagerPtr getChunkManager( const Shard& shard );
         DBClientCursorPtr getShardCursor( const Shard& shard );
 
@@ -338,10 +352,10 @@ namespace mongo {
 
         virtual void _explain( map< string,list<BSONObj> >& out );
 
-        bool _isCommand(){ return ! _cInfo.isEmpty(); }
         void _markStaleNS( const NamespaceString& staleNS, bool& forceReload, bool& fullReload );
         void _handleStaleNS( const NamespaceString& staleNS, bool forceReload, bool fullReload );
 
+        set<Shard> _qShards;
         QuerySpec _qSpec;
         CommandInfo _cInfo;
 

s/strategy_shard.cpp
@@ -99,6 +99,27 @@ namespace mongo {
             }
         }
 
+        virtual void commandOp( const string& db, const BSONObj& command, int options,
+                                const string& versionedNS, const BSONObj& filter,
+                                map<Shard,BSONObj>& results )
+        {
+
+            QuerySpec qSpec( db + ".$cmd", command, BSONObj(), 0, 1, options );
+
+            ParallelSortClusteredCursor cursor( qSpec, CommandInfo( versionedNS, filter ) );
+
+            // Initialize the cursor
+            cursor.init();
+
+            set<Shard> shards;
+            cursor.getQueryShards( shards );
+
+            for( set<Shard>::iterator i = shards.begin(), end = shards.end(); i != end; ++i ){
+                results[ *i ] = cursor.getShardCursor( *i )->peekFirst().getOwned();
+            }
+
+        }
+
         virtual void getMore( Request& r ) {
 
             // TODO:  Handle stale config exceptions here from coll being dropped or sharded during op
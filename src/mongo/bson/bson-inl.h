MitsukiSUGITA/my_mongoDB

commit 9c2d35067441e0654e827a174fa4cba4b8d25ad2

SERVER-5127 Limit recursion depth
This is to safeguard against a scenario where a deep nested array
can crash BSONObj.toString() and BSONObjElement.toString()
which eventually was crashing mongod.

src/mongo/bson/bson-inl.h
@@ -23,6 +23,7 @@
 #include <map>
 #include <limits>
 
+
 #if defined(_WIN32)
 #undef max
 #undef min
@@ -465,7 +466,7 @@ dodouble:
         toString(s, isArray, full);
         return s.str();
     }
-    inline void BSONObj::toString(StringBuilder& s,  bool isArray, bool full ) const {
+    inline void BSONObj::toString( StringBuilder& s,  bool isArray, bool full, int depth ) const {
         if ( isEmpty() ) {
             s << "{}";
             return;
@@ -492,7 +493,7 @@ dodouble:
                 first = false;
             else
                 s << ", ";
-            e.toString(s, !isArray, full );
+            e.toString( s, !isArray, full, depth );
         }
         s << ( isArray ? " ]" : " }" );
     }
@@ -689,7 +690,15 @@ dodouble:
         toString(s, includeFieldName, full);
         return s.str();
     }
-    inline void BSONElement::toString(StringBuilder& s, bool includeFieldName, bool full ) const {
+    inline void BSONElement::toString( StringBuilder& s, bool includeFieldName, bool full, int depth ) const {
+
+        if ( depth > BSONObj::maxToStringRecursionDepth ) {
+            // check if we want the full/complete string
+            uassert(16146, "Reached maximum recursion depth of " + BSONObjBuilder::numStr(BSONObj::maxToStringRecursionDepth), full != true);
+            s << "...";
+            return;
+        }
+
         if ( includeFieldName && type() != EOO )
             s << fieldName() << ": ";
         switch ( type() ) {
@@ -718,10 +727,10 @@ dodouble:
             s << ( boolean() ? "true" : "false" );
             break;
         case Object:
-            embeddedObject().toString(s, false, full);
+            embeddedObject().toString(s, false, full, depth+1);
             break;
         case mongo::Array:
-            embeddedObject().toString(s, true, full);
+            embeddedObject().toString(s, true, full, depth+1);
             break;
         case Undefined:
             s << "undefined";

src/mongo/bson/bsonelement.h
@@ -100,7 +100,7 @@ namespace mongo {
         bool ok() const { return !eoo(); }
 
         std::string toString( bool includeFieldName = true, bool full=false) const;
-        void toString(StringBuilder& s, bool includeFieldName = true, bool full=false) const;
+        void toString(StringBuilder& s, bool includeFieldName = true, bool full=false, int depth=0) const;
         std::string jsonString( JsonStringFormat format, bool includeFieldNames = true, int pretty = 0 ) const;
         operator std::string() const { return toString(); }
 

src/mongo/bson/bsonobj.h
@@ -138,8 +138,10 @@ namespace mongo {
         /** Readable representation of a BSON object in an extended JSON-style notation.
             This is an abbreviated representation which might be used for logging.
         */
+        enum { maxToStringRecursionDepth = 500 };
+
         std::string toString( bool isArray = false, bool full=false ) const;
-        void toString(StringBuilder& s, bool isArray = false, bool full=false ) const;
+        void toString( StringBuilder& s, bool isArray = false, bool full=false, int depth=0 ) const;
 
         /** Properly formatted JSON string.
             @param pretty if true we try to add some lf's and indentation

src/mongo/dbtests/jsobjtests.cpp
@@ -808,6 +808,57 @@ namespace JsobjTests {
             }
         };
 
+        class ToStringRecursionDepth {
+        public:
+            // create a nested BSON object with the specified recursion depth
+            BSONObj recursiveBSON( int depth )  {
+                BSONObjBuilder b;
+                if ( depth==0 ) {
+                    b << "name" << "Joe";
+                    return b.obj();
+                }
+                b.append( "test", recursiveBSON( depth - 1) );
+                return b.obj();
+            }
+
+            void run() {
+                BSONObj nestedBSON;
+                StringBuilder s;
+                string nestedBSONString;
+                size_t found;
+
+                // recursion depth one less than max allowed-- do not shorten the string
+                nestedBSON = recursiveBSON( BSONObj::maxToStringRecursionDepth - 1  );
+                nestedBSON.toString( s, true, false );
+                nestedBSONString = s.str();
+                found = nestedBSONString.find( "..." );
+                // did not find the "..." pattern
+                ASSERT_EQUALS( found!=string::npos, false );
+
+                // recursion depth is equal to max allowed  -- do not shorten the string
+                nestedBSON = recursiveBSON( BSONObj::maxToStringRecursionDepth );
+                nestedBSON.toString( s, true, false );
+                nestedBSONString = s.str();
+                found = nestedBSONString.find( "..." );
+                // did not find the "..." pattern
+                ASSERT_EQUALS( found!=string::npos, false );
+
+                // recursion depth - one greater than max allowed -- shorten the string
+                nestedBSON = recursiveBSON( BSONObj::maxToStringRecursionDepth + 1 );
+                nestedBSON.toString( s, false, false );
+                nestedBSONString = s.str();
+                found = nestedBSONString.find( "..." );
+                // found the "..." pattern
+                ASSERT_EQUALS( found!=string::npos, true );
+
+                /* recursion depth - one greater than max allowed but with full=true
+                 * should fail with an assertion
+                 */
+                nestedBSON = recursiveBSON( BSONObj::maxToStringRecursionDepth + 1 );
+                ASSERT_THROWS( nestedBSON.toString( s, false, true ) , UserException );
+            }
+        };
+
         namespace Validation {
 
             class Base {
@@ -2173,6 +2224,7 @@ namespace JsobjTests {
             add< BSONObjTests::AppendAs >();
             add< BSONObjTests::ArrayAppendAs >();
             add< BSONObjTests::GetField >();
+            add< BSONObjTests::ToStringRecursionDepth >();
 
             add< BSONObjTests::Validation::BadType >();
             add< BSONObjTests::Validation::EooBeforeEnd >();

commit ca9d99863b6fe08186cc4a18bc0a10670088e7c1

New message id in uassert
The previous commit 9c2d35067441e0654e827a174fa4cba4b8d25ad2
broke the build because of duplicate message id

src/mongo/bson/bson-inl.h
@@ -694,7 +694,7 @@ dodouble:
 
         if ( depth > BSONObj::maxToStringRecursionDepth ) {
             // check if we want the full/complete string
-            uassert(16146, "Reached maximum recursion depth of " + BSONObjBuilder::numStr(BSONObj::maxToStringRecursionDepth), full != true);
+            uassert(16150, "Reached maximum recursion depth of " + BSONObjBuilder::numStr(BSONObj::maxToStringRecursionDepth), full != true);
             s << "...";
             return;
         }
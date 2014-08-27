MitsukiSUGITA/my_mongoDB

commit 0e33ee34fed7977a9e22470bd8014a510373b1eb

SERVER-14668 Remove d_globals.h/.cpp
They were only used for ClientCursorMonitor startup, so I moved this to be
a singleton task.

src/mongo/db/clientcursor.cpp
@@ -222,23 +222,32 @@ namespace mongo {
     // ClientCursorMonitor
     //
 
-    void ClientCursorMonitor::run() {
-        Client::initThread("clientcursormon");
-        Client& client = cc();
-        Timer t;
-        const int Secs = 4;
-        while (!inShutdown()) {
-            OperationContextImpl txn;
-            cursorStatsTimedOut.increment(
-                        CollectionCursorCache::timeoutCursorsGlobal(&txn, t.millisReset()));
-            sleepsecs(Secs);
+    /**
+     * Thread for timing out old cursors
+     */
+    class ClientCursorMonitor : public BackgroundJob {
+    public:
+        std::string name() const { return "ClientCursorMonitor"; }
+
+        void run() {
+            Client::initThread("clientcursormon");
+            Client& client = cc();
+            Timer t;
+            const int Secs = 4;
+            while (!inShutdown()) {
+                OperationContextImpl txn;
+                cursorStatsTimedOut.increment(
+                    CollectionCursorCache::timeoutCursorsGlobal(&txn, t.millisReset()));
+                sleepsecs(Secs);
+            }
+            client.shutdown();
         }
-        client.shutdown();
-    }
+    };
 
     namespace {
-        ClientCursorMonitor clientCursorMonitor;
-        
+        // Only one instance of the ClientCursorMonitor exists
+        ClientCursorMonitor clientCursorMonitor = ClientCursorMonitor();
+
         void _appendCursorStats( BSONObjBuilder& b ) {
             b.append( "note" , "deprecated, use server status metrics" );
             b.appendNumber("clientCursors_size", cursorStatsOpen.get() );
@@ -247,6 +256,10 @@ namespace mongo {
             b.appendNumber("totalNoTimeout", cursorStatsOpenNoTimeout.get() );
             b.appendNumber("timedOut" , cursorStatsTimedOut.get());
         }
+    }
+
+    void startClientCursorMonitor() {
+        clientCursorMonitor.go();
     }
 
     // QUESTION: Restrict to the namespace from which this command was issued?

src/mongo/SConscript
@@ -560,7 +560,6 @@ env.Library('global_environment_experiment',
 # libs.
 serverOnlyFiles = [ "db/curop.cpp",
                     "db/global_environment_d.cpp",
-                    "db/d_globals.cpp",
                     "util/compress.cpp",
                     "db/ttl.cpp",
                     "util/logfile.cpp",

src/mongo/db/clientcursor.h
@@ -225,11 +225,6 @@ namespace mongo {
         ClientCursor* _cursor;
     };
 
-    /** thread for timing out old cursors */
-    class ClientCursorMonitor : public BackgroundJob {
-    public:
-        std::string name() const { return "ClientCursorMonitor"; }
-        void run();
-    };
+    void startClientCursorMonitor();
 
 } // namespace mongo

src/mongo/db/concurrency/d_concurrency.cpp
@@ -35,7 +35,6 @@
 #include "mongo/db/client.h"
 #include "mongo/db/commands/server_status.h"
 #include "mongo/db/curop.h"
-#include "mongo/db/d_globals.h"
 #include "mongo/db/global_environment_experiment.h"
 #include "mongo/db/concurrency/lock_stat.h"
 #include "mongo/db/server_parameters.h"

src/mongo/db/d_globals.cpp
@@ -1,47 +1,0 @@
-// @file d_globals.cpp
-
-/**
-*    Copyright (C) 2012 10gen Inc.
-*
-*    This program is free software: you can redistribute it and/or  modify
-*    it under the terms of the GNU Affero General Public License, version 3,
-*    as published by the Free Software Foundation.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Affero General Public License for more details.
-*
-*    You should have received a copy of the GNU Affero General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*    As a special exception, the copyright holders give permission to link the
-*    code of portions of this program with the OpenSSL library under certain
-*    conditions as described in each individual source file and distribute
-*    linked combinations including the program with the OpenSSL library. You
-*    must comply with the GNU Affero General Public License in all respects for
-*    all of the code used other than as permitted herein. If you modify file(s)
-*    with this exception, you may extend this exception to your version of the
-*    file(s), but you are not obligated to do so. If you do not wish to do so,
-*    delete this exception statement from your version. If you delete this
-*    exception statement from all source files in the program, then also delete
-*    it in the license file.
-*/
-
-#include "mongo/pch.h"
-
-#include "mongo/db/d_globals.h"
-
-#include "mongo/db/clientcursor.h"
-#include "mongo/util/concurrency/rwlock.h"
-
-namespace mongo { 
-
-    DGlobals::DGlobals() :
-        clientCursorMonitor( *(new ClientCursorMonitor()) )
-    {
-    }
-
-    DGlobals d;
-
-}

src/mongo/db/d_globals.h
@@ -1,53 +1,0 @@
-// @file d_globals.h
-
-/**
-*    Copyright (C) 2012 10gen Inc.
-*
-*    This program is free software: you can redistribute it and/or  modify
-*    it under the terms of the GNU Affero General Public License, version 3,
-*    as published by the Free Software Foundation.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Affero General Public License for more details.
-*
-*    You should have received a copy of the GNU Affero General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*    As a special exception, the copyright holders give permission to link the
-*    code of portions of this program with the OpenSSL library under certain
-*    conditions as described in each individual source file and distribute
-*    linked combinations including the program with the OpenSSL library. You
-*    must comply with the GNU Affero General Public License in all respects for
-*    all of the code used other than as permitted herein. If you modify file(s)
-*    with this exception, you may extend this exception to your version of the
-*    file(s), but you are not obligated to do so. If you do not wish to do so,
-*    delete this exception statement from your version. If you delete this
-*    exception statement from all source files in the program, then also delete
-*    it in the license file.
-*/
-
-//
-// these are global variables used in mongod ("d").  also used in test binary as that is effectively a variation on mongod code.
-// that is, these are not in mongos.
-//
-
-#pragma once
-
-namespace mongo { 
-
-    class RWLockRecursive;
-    class ClientCursorMonitor;
-
-    struct DGlobals : boost::noncopyable { 
-        DGlobals();
-
-        // these are intentionally never deleted:
-        ClientCursorMonitor& clientCursorMonitor;
-
-    };
-
-    extern DGlobals d;
-
-};

src/mongo/db/db.cpp
@@ -52,7 +52,6 @@
 #include "mongo/db/commands/server_status.h"
 #include "mongo/db/commands/server_status_metric.h"
 #include "mongo/db/concurrency/d_concurrency.h"
-#include "mongo/db/d_globals.h"
 #include "mongo/db/db.h"
 #include "mongo/db/dbmessage.h"
 #include "mongo/db/dbwebserver.h"
@@ -641,7 +640,8 @@ namespace mongo {
         if (serverGlobalParams.isHttpInterfaceEnabled)
             snapshotThread.go();
 
-        d.clientCursorMonitor.go();
+        startClientCursorMonitor();
+
         PeriodicTask::startRunningPeriodicTasks();
         if (missingRepl) {
             // a warning was logged earlier

commit 1bb33aeab23d214c057f8eae3a299abb6320867a

SERVER-14668 Fix for commit 0e33ee34fe which broke OSX build

src/mongo/db/clientcursor.cpp
@@ -246,7 +246,7 @@ namespace mongo {
 
     namespace {
         // Only one instance of the ClientCursorMonitor exists
-        ClientCursorMonitor clientCursorMonitor = ClientCursorMonitor();
+        ClientCursorMonitor clientCursorMonitor;
 
         void _appendCursorStats( BSONObjBuilder& b ) {
             b.append( "note" , "deprecated, use server status metrics" );
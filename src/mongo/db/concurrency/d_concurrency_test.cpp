MitsukiSUGITA/my_mongoDB

commit b61361d6fbf8e40331ceb8cfefea46a74ebb8ec8

SERVER-14412 Beginnings of a collection-level locking API for non-MMAP V1 engines
This commit adds capability to the storage engine to declare whether it
supports document-level locking or not. If document-level locking is not
supported, MongoDB will lock down to the collection level (not yet
implemented).

src/mongo/db/concurrency/d_concurrency.cpp
@@ -37,6 +37,8 @@
 #include "mongo/db/concurrency/locker.h"
 #include "mongo/db/concurrency/lock_stat.h"
 #include "mongo/db/concurrency/lock_state.h"
+#include "mongo/db/global_environment_experiment.h"
+#include "mongo/db/namespace_string.h"
 #include "mongo/db/server_parameters.h"
 #include "mongo/db/operation_context.h"
 #include "mongo/util/assert_util.h"
@@ -95,6 +97,23 @@ namespace mongo {
         Locker* const _ls;
     };
 
+namespace {
+    /**
+     * Shortcut for querying the storage engine if it supports document-level locking. If this
+     * call becomes too expensive, we could cache the value somewhere so we don't have to fetch
+     * the storage engine every time.
+     */
+    bool supportsDocLocking() {
+        if (hasGlobalEnvironment()) {
+            StorageEngine* globalStorageEngine = getGlobalEnvironment()->getGlobalStorageEngine();
+            if (globalStorageEngine != NULL) {
+                return globalStorageEngine->supportsDocLocking();
+            }
+        }
+
+        return false;
+    }
+}
 
     RWLockRecursive &Lock::ParallelBatchWriterMode::_batchLock = *(new RWLockRecursive("special"));
     void Lock::ParallelBatchWriterMode::iAmABatchParticipant(Locker* lockState) {
@@ -283,7 +302,19 @@ namespace mongo {
         const newlm::ResourceId resIdDb(newlm::RESOURCE_DATABASE, db);
 
         _lockState->lockGlobal(newlm::MODE_IX);
-        _lockState->lock(resIdDb, newlm::MODE_X);
+
+        if (supportsDocLocking()) {
+            _lockState->lock(resIdDb, newlm::MODE_IX);
+
+            const StringData ns = nsToCollectionSubstring(_ns);
+            if (!ns.empty()) {
+                const newlm::ResourceId resIdCollection(newlm::RESOURCE_COLLECTION, db);
+                _lockState->lock(resIdCollection, newlm::MODE_IX);
+            }
+        }
+        else {
+            _lockState->lock(resIdDb, newlm::MODE_X);
+        }
 
         resetTime();
     }
@@ -320,7 +351,19 @@ namespace mongo {
         const newlm::ResourceId resIdDb(newlm::RESOURCE_DATABASE, db);
 
         _lockState->lockGlobal(newlm::MODE_IS);
-        _lockState->lock(resIdDb, newlm::MODE_S);
+
+        if (supportsDocLocking()) {
+            _lockState->lock(resIdDb, newlm::MODE_IS);
+
+            const StringData ns = nsToCollectionSubstring(_ns);
+            if (!ns.empty()) {
+                const newlm::ResourceId resIdCollection(newlm::RESOURCE_COLLECTION, db);
+                _lockState->lock(resIdCollection, newlm::MODE_IS);
+            }
+        }
+        else {
+            _lockState->lock(resIdDb, newlm::MODE_X);
+        }
 
         resetTime();
     }

src/mongo/db/storage/heap1/heap1_engine.h
@@ -52,6 +52,8 @@ namespace mongo {
         virtual DatabaseCatalogEntry* getDatabaseCatalogEntry( OperationContext* opCtx,
                                                                const StringData& db );
 
+        virtual bool supportsDocLocking() const { return false; }
+
         virtual Status closeDatabase(OperationContext* txn, const StringData& db );
 
         virtual Status dropDatabase(OperationContext* txn, const StringData& db );

src/mongo/db/storage/mmap_v1/mmap_v1_engine.h
@@ -59,6 +59,8 @@ namespace mongo {
         DatabaseCatalogEntry* getDatabaseCatalogEntry( OperationContext* opCtx,
                                                        const StringData& db );
 
+        virtual bool supportsDocLocking() const { return false; }
+
         Status closeDatabase(OperationContext* txn, const StringData& db );
 
         Status dropDatabase(OperationContext* txn, const StringData& db );

src/mongo/db/storage/rocks/rocks_engine.h
@@ -82,6 +82,8 @@ namespace mongo {
         virtual DatabaseCatalogEntry* getDatabaseCatalogEntry( OperationContext* opCtx,
                                                                const StringData& db );
 
+        virtual bool supportsDocLocking() const { return false; }
+
         /**
          * @return number of files flushed
          */

src/mongo/db/storage/storage_engine.h
@@ -99,6 +99,15 @@ namespace mongo {
                                                                const StringData& db ) = 0;
 
         /**
+         * Returns whether the storage engine supports its own locking locking below the collection
+         * level. If the engine returns true, MongoDB will acquire intent locks down to the
+         * collection level and will assume that the engine will ensure consistency at the level of
+         * documents. If false, MongoDB will lock the entire collection in Shared/Exclusive mode
+         * for read/write operations respectively.
+         */
+        virtual bool supportsDocLocking() const = 0;
+
+        /**
          * Closes all file handles associated with a database.
          */
         virtual Status closeDatabase( OperationContext* txn, const StringData& db ) = 0;

commit 59f5820308409041b8facc847d247b94600728c9

SERVER-14668 DBRead should be acquiring S lock, not X
This fixes a bug introduced in commit
b61361d6fbf8e40331ceb8cfefea46a74ebb8ec8.

src/mongo/db/concurrency/d_concurrency.cpp
@@ -367,7 +367,7 @@ namespace {
             }
         }
         else {
-            _lockState->lock(resIdDb, newlm::MODE_X);
+            _lockState->lock(resIdDb, newlm::MODE_S);
         }
 
         resetTime();

src/mongo/db/concurrency/d_concurrency_test.cpp
@@ -134,6 +134,24 @@ namespace mongo {
         ASSERT(ls.isW());
     }
 
+    TEST(DConcurrency, DBReadTakesS) {
+        LockState ls;
+
+        Lock::DBRead dbRead(&ls, "db");
+
+        const newlm::ResourceId resIdDb(newlm::RESOURCE_DATABASE, string("db"));
+        ASSERT(ls.getLockMode(resIdDb) == newlm::MODE_S);
+    }
+
+    TEST(DConcurrency, DBWriteTakesX) {
+        LockState ls;
+
+        Lock::DBWrite dbWrite(&ls, "db");
+
+        const newlm::ResourceId resIdDb(newlm::RESOURCE_DATABASE, string("db"));
+        ASSERT(ls.getLockMode(resIdDb) == newlm::MODE_X);
+    }
+
     TEST(DConcurrency, MultipleWriteDBLocksOnSameThread) {
         LockState ls;
 
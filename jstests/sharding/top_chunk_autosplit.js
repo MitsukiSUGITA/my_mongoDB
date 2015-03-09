MitsukiSUGITA/my_mongoDB

commit c1df4691f784a2f48f93ba0d07c7c4bf914d6d6b

SERVER-17310 Replace boost::*_mutex::scoped_lock with boost::lock_guard.
Achieved via grep, sed and bash:
grep -Irl mutex::scoped_lock src/mongo | xargs sed -i.orig -E 's/(boost::(recursive_|timed_)?)mutex::scoped_lock/boost::lock_guard<\1mutex>/'

Then, by converting boost::lock_guard to boost::unique_lock as appropriate.

Finally, by removing unused mongo::mutex::try_lock.

src/mongo/db/storage/rocks/rocks_record_store.cpp
@@ -83,7 +83,7 @@ namespace mongo {
 
     void CappedVisibilityManager::addUncommittedRecord(OperationContext* txn,
                                                        const RecordId& record) {
-        boost::mutex::scoped_lock lk(_lock);
+        boost::lock_guard<boost::mutex> lk(_lock);
         _addUncommittedRecord_inlock(txn, record);
     }
 
@@ -98,14 +98,14 @@ namespace mongo {
 
     RecordId CappedVisibilityManager::getNextAndAddUncommittedRecord(
         OperationContext* txn, std::function<RecordId()> nextId) {
-        boost::mutex::scoped_lock lk(_lock);
+        boost::lock_guard<boost::mutex> lk(_lock);
         RecordId record = nextId();
         _addUncommittedRecord_inlock(txn, record);
         return record;
     }
 
     void CappedVisibilityManager::dealtWithCappedRecord(const RecordId& record) {
-        boost::mutex::scoped_lock lk(_lock);
+        boost::lock_guard<boost::mutex> lk(_lock);
         std::vector<RecordId>::iterator it =
             std::find(_uncommittedRecords.begin(), _uncommittedRecords.end(), record);
         invariant(it != _uncommittedRecords.end());
@@ -113,7 +113,7 @@ namespace mongo {
     }
 
     bool CappedVisibilityManager::isCappedHidden(const RecordId& record) const {
-        boost::mutex::scoped_lock lk(_lock);
+        boost::lock_guard<boost::mutex> lk(_lock);
         if (_uncommittedRecords.empty()) {
             return false;
         }
@@ -122,7 +122,7 @@ namespace mongo {
 
     void CappedVisibilityManager::updateHighestSeen(const RecordId& record) {
         if (record > _oplog_highestSeen) {
-            boost::mutex::scoped_lock lk(_lock);
+            boost::lock_guard<boost::mutex> lk(_lock);
             if (record > _oplog_highestSeen) {
                 _oplog_highestSeen = record;
             }
@@ -130,7 +130,7 @@ namespace mongo {
     }
 
     RecordId CappedVisibilityManager::oplogStartHack() const {
-        boost::mutex::scoped_lock lk(_lock);
+        boost::lock_guard<boost::mutex> lk(_lock);
         if (_uncommittedRecords.empty()) {
             return _oplog_highestSeen;
         } else {
@@ -197,7 +197,7 @@ namespace mongo {
 
     RocksRecordStore::~RocksRecordStore() {
         {
-            boost::timed_mutex::scoped_lock lk(_cappedDeleterMutex);
+            boost::lock_guard<boost::timed_mutex> lk(_cappedDeleterMutex);
             _shuttingDown = true;
         }
     }
@@ -276,7 +276,7 @@ namespace mongo {
         }
 
         // ensure only one thread at a time can do deletes, otherwise they'll conflict.
-        boost::timed_mutex::scoped_lock lock(_cappedDeleterMutex, boost::defer_lock);
+        boost::lock_guard<boost::timed_mutex> lock(_cappedDeleterMutex, boost::defer_lock);
 
         if (_cappedMaxDocs != -1) {
             lock.lock(); // Max docs has to be exact, so have to check every time.

src/mongo/db/storage/rocks/rocks_transaction.cpp
@@ -89,7 +89,7 @@ namespace mongo {
         }
         uint64_t newSnapshotId = 0;
         {
-            boost::mutex::scoped_lock lk(_transactionEngine->_lock);
+            boost::lock_guard<boost::mutex> lk(_transactionEngine->_lock);
             for (const auto& key : _writtenKeys) {
                 invariant(
                     !_transactionEngine->_isKeyCommittedAfterSnapshot_inlock(key, _snapshotId));
@@ -108,7 +108,7 @@ namespace mongo {
     }
 
     bool RocksTransaction::registerWrite(const std::string& key) {
-        boost::mutex::scoped_lock lk(_transactionEngine->_lock);
+        boost::lock_guard<boost::mutex> lk(_transactionEngine->_lock);
         if (_transactionEngine->_isKeyCommittedAfterSnapshot_inlock(key, _snapshotId)) {
             // write-committed write conflict
             return false;
@@ -129,7 +129,7 @@ namespace mongo {
             return;
         }
         {
-            boost::mutex::scoped_lock lk(_transactionEngine->_lock);
+            boost::lock_guard<boost::mutex> lk(_transactionEngine->_lock);
             for (const auto& key : _writtenKeys) {
                 _transactionEngine->_uncommittedTransactionId.erase(key);
             }
@@ -140,7 +140,7 @@ namespace mongo {
 
     void RocksTransaction::recordSnapshotId() {
         {
-            boost::mutex::scoped_lock lk(_transactionEngine->_lock);
+            boost::lock_guard<boost::mutex> lk(_transactionEngine->_lock);
             _cleanup_inlock();
             _activeSnapshotsIter = _transactionEngine->_getLatestSnapshotId_inlock();
         }

src/mongo/client/replica_set_monitor.cpp
@@ -284,7 +284,7 @@ namespace {
 
     HostAndPort ReplicaSetMonitor::getHostOrRefresh(const ReadPreferenceSetting& criteria) {
         {
-            boost::mutex::scoped_lock lk(_state->mutex);
+            boost::lock_guard<boost::mutex> lk(_state->mutex);
             HostAndPort out = _state->getMatchingHost(criteria);
             if (!out.empty())
                 return out;
@@ -313,7 +313,7 @@ namespace {
     }
 
     Refresher ReplicaSetMonitor::startOrContinueRefresh() {
-        boost::mutex::scoped_lock lk(_state->mutex);
+        boost::lock_guard<boost::mutex> lk(_state->mutex);
 
         Refresher out(_state);
         DEV _state->checkInvariants();
@@ -321,7 +321,7 @@ namespace {
     }
 
     void ReplicaSetMonitor::failedHost(const HostAndPort& host) {
-        boost::mutex::scoped_lock lk(_state->mutex);
+        boost::lock_guard<boost::mutex> lk(_state->mutex);
         Node* node = _state->findNode(host);
         if (node)
             node->markFailed();
@@ -329,19 +329,19 @@ namespace {
     }
 
     bool ReplicaSetMonitor::isPrimary(const HostAndPort& host) const {
-        boost::mutex::scoped_lock lk(_state->mutex);
+        boost::lock_guard<boost::mutex> lk(_state->mutex);
         Node* node = _state->findNode(host);
         return node ? node->isMaster : false;
     }
 
     bool ReplicaSetMonitor::isHostUp(const HostAndPort& host) const {
-        boost::mutex::scoped_lock lk(_state->mutex);
+        boost::lock_guard<boost::mutex> lk(_state->mutex);
         Node* node = _state->findNode(host);
         return node ? node->isUp : false;
     }
 
     int ReplicaSetMonitor::getConsecutiveFailedScans() const {
-        boost::mutex::scoped_lock lk(_state->mutex);
+        boost::lock_guard<boost::mutex> lk(_state->mutex);
         return _state->consecutiveFailedScans;
     }
 
@@ -351,12 +351,12 @@ namespace {
     }
 
     std::string ReplicaSetMonitor::getServerAddress() const {
-        boost::mutex::scoped_lock lk(_state->mutex);
+        boost::lock_guard<boost::mutex> lk(_state->mutex);
         return _state->getServerAddress();
     }
 
     bool ReplicaSetMonitor::contains(const HostAndPort& host) const {
-        boost::mutex::scoped_lock lk(_state->mutex);
+        boost::lock_guard<boost::mutex> lk(_state->mutex);
         return _state->seedNodes.count(host);
     }
 
@@ -413,7 +413,7 @@ namespace {
             if (!clearSeedCache) {
                 // Save list of current set members so that the monitor can be rebuilt if needed.
                 const ReplicaSetMonitorPtr& rsm = setIt->second;
-                boost::mutex::scoped_lock lk(rsm->_state->mutex);
+                boost::lock_guard<boost::mutex> lk(rsm->_state->mutex);
                 seedServers[name] = rsm->_state->seedNodes;
             }
             sets.erase(setIt);
@@ -436,7 +436,7 @@ namespace {
 
     // TODO move to correct order with non-statics before pushing
     void ReplicaSetMonitor::appendInfo(BSONObjBuilder& bsonObjBuilder) const {
-        boost::mutex::scoped_lock lk(_state->mutex);
+        boost::lock_guard<boost::mutex> lk(_state->mutex);
 
         // NOTE: the format here must be consistent for backwards compatibility
         BSONArrayBuilder hosts(bsonObjBuilder.subarrayStart("hosts"));
@@ -746,7 +746,7 @@ namespace {
     }
 
     HostAndPort Refresher::_refreshUntilMatches(const ReadPreferenceSetting* criteria) {
-        boost::mutex::scoped_lock lk(_set->mutex);
+        boost::unique_lock<boost::mutex> lk(_set->mutex);
         while (true) {
             if (criteria) {
                 HostAndPort out = _set->getMatchingHost(*criteria);

src/mongo/db/auth/internal_user_auth.cpp
@@ -59,7 +59,7 @@ namespace mongo {
         if (!isInternalAuthSet()) {
             authParamsSet = true;
         }
-        boost::mutex::scoped_lock lk(authParamMutex);
+        boost::lock_guard<boost::mutex> lk(authParamMutex);
 
         if (authParamsIn["mechanism"].String() != "SCRAM-SHA-1") {
             authParams = authParamsIn.copy();
@@ -83,7 +83,7 @@ namespace mongo {
             return BSONObj();
         }
 
-        boost::mutex::scoped_lock lk(authParamMutex);
+        boost::lock_guard<boost::mutex> lk(authParamMutex);
         return authParams.copy();
     }
 

src/mongo/db/background.cpp
@@ -119,12 +119,12 @@ namespace {
 
 }  // namespace
     bool BackgroundOperation::inProgForDb(StringData db) {
-        boost::mutex::scoped_lock lk(m);
+        boost::lock_guard<boost::mutex> lk(m);
         return dbsInProg.find(db) != dbsInProg.end();
     }
 
     bool BackgroundOperation::inProgForNs(StringData ns) {
-        boost::mutex::scoped_lock lk(m);
+        boost::lock_guard<boost::mutex> lk(m);
         return nsInProg.find(ns) != nsInProg.end();
     }
 
@@ -153,19 +153,19 @@ namespace {
     }
 
     BackgroundOperation::BackgroundOperation(StringData ns) : _ns(ns) {
-        boost::mutex::scoped_lock lk(m);
+        boost::lock_guard<boost::mutex> lk(m);
         recordBeginAndInsert(&dbsInProg, _ns.db());
         recordBeginAndInsert(&nsInProg, _ns.ns());
     }
 
     BackgroundOperation::~BackgroundOperation() {
-        boost::mutex::scoped_lock lk(m);
+        boost::lock_guard<boost::mutex> lk(m);
         recordEndAndRemove(&dbsInProg, _ns.db());
         recordEndAndRemove(&nsInProg, _ns.ns());
     }
 
     void BackgroundOperation::dump(std::ostream& ss) {
-        boost::mutex::scoped_lock lk(m);
+        boost::lock_guard<boost::mutex> lk(m);
         if( nsInProg.size() ) {
             ss << "\n<b>Background Jobs in Progress</b>\n";
             for( BgInfoMapIterator i = nsInProg.begin(); i != nsInProg.end(); ++i )

src/mongo/db/client.cpp
@@ -120,7 +120,7 @@ namespace {
         currentClient.reset(client);
 
         // This makes the client visible to maintenance threads
-        boost::mutex::scoped_lock clientLock(clientsMutex);
+        boost::lock_guard<boost::mutex> clientLock(clientsMutex);
         clients.insert(client);
     }
 
@@ -143,7 +143,7 @@ namespace {
         if ( ! inShutdown() ) {
             // we can't clean up safely once we're in shutdown
             {
-                boost::mutex::scoped_lock clientLock(clientsMutex);
+                boost::lock_guard<boost::mutex> clientLock(clientsMutex);
                 if ( ! _shutdown )
                     clients.erase(this);
             }
@@ -162,7 +162,7 @@ namespace {
         if ( inShutdown() )
             return false;
         {
-            boost::mutex::scoped_lock clientLock(clientsMutex);
+            boost::lock_guard<boost::mutex> clientLock(clientsMutex);
             clients.erase(this);
         }
 

src/mongo/db/clientcursor.h
@@ -42,7 +42,7 @@
 
 namespace mongo {
 
-    typedef boost::recursive_mutex::scoped_lock recursive_scoped_lock;
+    typedef boost::lock_guard<boost::recursive_mutex> recursive_scoped_lock;
     class ClientCursor;
     class Collection;
     class CurOp;

src/mongo/db/clientlistplugin.cpp
@@ -86,7 +86,7 @@ namespace {
         static void _processAllClients(std::stringstream& ss) {
             using namespace html;
 
-            boost::mutex::scoped_lock scopedLock(Client::clientsMutex);
+            boost::lock_guard<boost::mutex> scopedLock(Client::clientsMutex);
 
             ClientSet::const_iterator it = Client::clients.begin();
             for (; it != Client::clients.end(); it++) {
@@ -198,7 +198,7 @@ namespace {
         static BSONArray _processAllClients(MatchExpression* matcher) {
             BSONArrayBuilder array;
 
-            boost::mutex::scoped_lock scopedLock(Client::clientsMutex);
+            boost::lock_guard<boost::mutex> scopedLock(Client::clientsMutex);
 
             ClientSet::const_iterator it = Client::clients.begin();
             for (; it != Client::clients.end(); it++) {

commit 8cea8acbee03b4aca63940eb3947b5dbca41cfbb

SERVER-17492 [RocksDB] Fix RocksDB build, broken by c1df469
Signed-off-by: Ramon Fernandez <ramon@mongodb.com>

jstests/sharding/top_chunk_autosplit.js
@@ -312,7 +312,7 @@ var maxSizeTests = [
 
 // Execute all test objects
 // SERVER-17070 Auto split moves to shard node running WiredTiger, if exceeding maxSize
-var unsupported = ["wiredTiger", "rocksExperiment"];
+var unsupported = ["wiredTiger", "RocksDB"];
 if (unsupported.indexOf(st.d0.adminCommand({serverStatus : 1}).storageEngine.name) == -1 &&
     unsupported.indexOf(st.d1.adminCommand({serverStatus : 1}).storageEngine.name) == -1) {
     for (var i = 0; i < maxSizeTests.length; i++) {

src/mongo/db/storage/rocks/rocks_record_store.cpp
@@ -35,6 +35,7 @@
 
 #include <boost/scoped_array.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/thread/locks.hpp>
 #include <memory>
 #include <algorithm>
 #include <utility>
@@ -276,7 +277,7 @@ namespace mongo {
         }
 
         // ensure only one thread at a time can do deletes, otherwise they'll conflict.
-        boost::lock_guard<boost::timed_mutex> lock(_cappedDeleterMutex, boost::defer_lock);
+        boost::unique_lock<boost::timed_mutex> lock(_cappedDeleterMutex, boost::defer_lock);
 
         if (_cappedMaxDocs != -1) {
             lock.lock(); // Max docs has to be exact, so have to check every time.

src/mongo/db/storage/rocks/rocks_record_store.h
@@ -32,6 +32,7 @@
 #include <atomic>
 #include <boost/scoped_ptr.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/thread/mutex.hpp>
 #include <string>
 #include <memory>
 #include <vector>

src/mongo/db/storage/rocks/rocks_transaction.cpp
@@ -35,6 +35,8 @@
 #include <memory>
 #include <string>
 
+#include <boost/thread/locks.hpp>
+
 // for invariant()
 #include "mongo/util/assert_util.h"
 
MitsukiSUGITA/my_mongoDB

commit 43b0568a7240c2ce9bd9f4a342968926ebf3858e

SERVER-16444 Copy data in the query subsystem as needed.
A WorkingSetMember in the LOC_AND_OBJ state must be made owned when:

  1. Its WorkingSetID is cached across multiple calls to work().

  2. Multiple calls to next(), seekExact(), saveState(), etc. are
     performed on the same WiredTiger cursor in a single work() call.

No longer necessary to always copy data out of WiredTiger buffers.

src/mongo/db/exec/and_common-inl.h
@@ -67,6 +67,7 @@ public:
 
             // 'src' has the full document but 'dest' doesn't so we need to copy it over.
             dest->obj = src.obj;
+            dest->makeObjOwned();
 
             // We have an object so we don't need key data.
             dest->keyData.clear();

src/mongo/db/exec/and_hash.cpp
@@ -136,6 +136,7 @@ PlanStage::StageState AndHashStage::work(WorkingSetID* out) {
         for (size_t i = 0; i < _children.size(); ++i) {
             auto& child = _children[i];
             for (size_t j = 0; j < kLookAheadWorks; ++j) {
+                // Cache the result in _lookAheadResults[i].
                 StageState childStatus = child->work(&_lookAheadResults[i]);
 
                 if (PlanStage::IS_EOF == childStatus) {
@@ -144,9 +145,10 @@ PlanStage::StageState AndHashStage::work(WorkingSetID* out) {
                     _dataMap.clear();
                     return PlanStage::IS_EOF;
                 } else if (PlanStage::ADVANCED == childStatus) {
-                    // We have a result cached in _lookAheadResults[i].  Stop looking at this
-                    // child.
-                    break;
+                    // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we
+                    // yield.
+                    _ws->get(_lookAheadResults[i])->makeObjOwned();
+                    break;  // Stop looking at this child.
                 } else if (PlanStage::FAILURE == childStatus || PlanStage::DEAD == childStatus) {
                     // Propage error to parent.
                     *out = _lookAheadResults[i];
@@ -282,6 +284,9 @@ PlanStage::StageState AndHashStage::readFirstChild(WorkingSetID* out) {
             ++_commonStats.needTime;
             return PlanStage::NEED_TIME;
         }
+
+        // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
+        member->makeObjOwned();
 
         // Update memory stats.
         _memUsage += member->getMemUsage();

src/mongo/db/exec/and_sorted.cpp
@@ -115,6 +115,9 @@ PlanStage::StageState AndSortedStage::getTargetLoc(WorkingSetID* out) {
         _targetId = id;
         _targetLoc = member->loc;
 
+        // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
+        member->makeObjOwned();
+
         // We have to AND with all other children.
         for (size_t i = 1; i < _children.size(); ++i) {
             _workingTowardRep.push(i);
@@ -210,6 +213,10 @@ PlanStage::StageState AndSortedStage::moveTowardTargetLoc(WorkingSetID* out) {
             _targetNode = workingChildNumber;
             _targetLoc = member->loc;
             _targetId = id;
+
+            // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
+            member->makeObjOwned();
+
             _workingTowardRep = std::queue<size_t>();
             for (size_t i = 0; i < _children.size(); ++i) {
                 if (workingChildNumber != i) {

src/mongo/db/exec/cached_plan.cpp
@@ -96,6 +96,9 @@ Status CachedPlanStage::pickBestPlan(PlanYieldPolicy* yieldPolicy) {
 
         if (PlanStage::ADVANCED == state) {
             // Save result for later.
+            WorkingSetMember* member = _ws->get(id);
+            // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
+            member->makeObjOwned();
             _results.push_back(id);
 
             if (_results.size() >= numResults) {

src/mongo/db/exec/delete.cpp
@@ -154,24 +154,27 @@ PlanStage::StageState DeleteStage::work(WorkingSetID* out) {
                 }
             }
 
+            // Ensure that the BSONObj underlying the WorkingSetMember is owned because saveState()
+            // is allowed to free the memory.
+            if (_params.returnDeleted) {
+                member->makeObjOwned();
+            }
+
             // TODO: Do we want to buffer docs and delete them in a group rather than
             // saving/restoring state repeatedly?
 
             try {
-                child()->saveState();
                 if (supportsDocLocking()) {
-                    // Doc-locking engines require this after saveState() since they don't use
+                    // Doc-locking engines require this before saveState() since they don't use
                     // invalidations.
                     WorkingSetCommon::prepareForSnapshotChange(_ws);
                 }
+                child()->saveState();
             } catch (const WriteConflictException& wce) {
                 std::terminate();
             }
 
             if (_params.returnDeleted) {
-                // Save a copy of the document that is about to get deleted.
-                BSONObj deletedDoc = member->obj.value();
-                member->obj.setValue(deletedDoc.getOwned());
                 member->loc = RecordId();
                 member->transitionToOwnedObj();
             }
@@ -185,6 +188,9 @@ PlanStage::StageState DeleteStage::work(WorkingSetID* out) {
 
             ++_specificStats.docsDeleted;
         } catch (const WriteConflictException& wce) {
+            // Ensure that the BSONObj underlying the WorkingSetMember is owned because it may be
+            // freed when we yield.
+            member->makeObjOwned();
             _idRetrying = id;
             memberFreer.Dismiss();  // Keep this member around so we can retry deleting it.
             *out = WorkingSet::INVALID_ID;

src/mongo/db/exec/fetch.cpp
@@ -128,6 +128,9 @@ PlanStage::StageState FetchStage::work(WorkingSetID* out) {
                     return NEED_TIME;
                 }
             } catch (const WriteConflictException& wce) {
+                // Ensure that the BSONObj underlying the WorkingSetMember is owned because it may
+                // be freed when we yield.
+                member->makeObjOwned();
                 _idRetrying = id;
                 *out = WorkingSet::INVALID_ID;
                 _commonStats.needYield++;

src/mongo/db/exec/merge_sort.cpp
@@ -87,10 +87,10 @@ PlanStage::StageState MergeSortStage::work(WorkingSetID* out) {
         StageState code = child->work(&id);
 
         if (PlanStage::ADVANCED == code) {
+            WorkingSetMember* member = _ws->get(id);
+
             // If we're deduping...
             if (_dedup) {
-                WorkingSetMember* member = _ws->get(id);
-
                 if (!member->hasLoc()) {
                     // Can't dedup data unless there's a RecordId.  We go ahead and use its
                     // result.
@@ -122,6 +122,8 @@ PlanStage::StageState MergeSortStage::work(WorkingSetID* out) {
             StageWithValue value;
             value.id = id;
             value.stage = child;
+            // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
+            member->makeObjOwned();
             _mergingData.push_front(value);
 
             // Insert the result (indirectly) into our priority queue.

src/mongo/db/exec/multi_plan.cpp
@@ -379,6 +379,10 @@ bool MultiPlanStage::workAllPlans(size_t numResults, PlanYieldPolicy* yieldPolic
 
         if (PlanStage::ADVANCED == state) {
             // Save result for later.
+            WorkingSetMember* member = candidate.ws->get(id);
+            // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we choose to
+            // return the results from the 'candidate' plan.
+            member->makeObjOwned();
             candidate.results.push_back(id);
 
             // Once a plan returns enough results, stop working.

commit 56ce904b4a0b52c1015915e1f2bbfa05f459c54e

SERVER-16444 Only make BSONObj underlying WSM owned if not mmapv1.
Resolves the performance regression with mmapv1 introduced by 43b0568
where documents were being copied in PlanStages that perform
invalidations.

Partially reverted changes to DeleteStage and UpdateStage because the
document to return is not guaranteed to be owned after
makeObjOwnedIfNeeded() is called when running with mmapv1.

src/mongo/db/exec/SConscript
@@ -10,6 +10,7 @@ env.Library(
     ],
     LIBDEPS = [
         "$BUILD_DIR/mongo/base",
+        "$BUILD_DIR/mongo/db/service_context",
     ],
 )
 

src/mongo/db/exec/and_common-inl.h
@@ -67,7 +67,7 @@ public:
 
             // 'src' has the full document but 'dest' doesn't so we need to copy it over.
             dest->obj = src.obj;
-            dest->makeObjOwned();
+            dest->makeObjOwnedIfNeeded();
 
             // We have an object so we don't need key data.
             dest->keyData.clear();

src/mongo/db/exec/and_hash.cpp
@@ -147,7 +147,7 @@ PlanStage::StageState AndHashStage::work(WorkingSetID* out) {
                 } else if (PlanStage::ADVANCED == childStatus) {
                     // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we
                     // yield.
-                    _ws->get(_lookAheadResults[i])->makeObjOwned();
+                    _ws->get(_lookAheadResults[i])->makeObjOwnedIfNeeded();
                     break;  // Stop looking at this child.
                 } else if (PlanStage::FAILURE == childStatus || PlanStage::DEAD == childStatus) {
                     // Propage error to parent.
@@ -286,7 +286,7 @@ PlanStage::StageState AndHashStage::readFirstChild(WorkingSetID* out) {
         }
 
         // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
-        member->makeObjOwned();
+        member->makeObjOwnedIfNeeded();
 
         // Update memory stats.
         _memUsage += member->getMemUsage();

src/mongo/db/exec/and_sorted.cpp
@@ -116,7 +116,7 @@ PlanStage::StageState AndSortedStage::getTargetLoc(WorkingSetID* out) {
         _targetLoc = member->loc;
 
         // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
-        member->makeObjOwned();
+        member->makeObjOwnedIfNeeded();
 
         // We have to AND with all other children.
         for (size_t i = 1; i < _children.size(); ++i) {
@@ -215,7 +215,7 @@ PlanStage::StageState AndSortedStage::moveTowardTargetLoc(WorkingSetID* out) {
             _targetId = id;
 
             // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
 
             _workingTowardRep = std::queue<size_t>();
             for (size_t i = 0; i < _children.size(); ++i) {

src/mongo/db/exec/cached_plan.cpp
@@ -98,7 +98,7 @@ Status CachedPlanStage::pickBestPlan(PlanYieldPolicy* yieldPolicy) {
             // Save result for later.
             WorkingSetMember* member = _ws->get(id);
             // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
             _results.push_back(id);
 
             if (_results.size() >= numResults) {

src/mongo/db/exec/delete.cpp
@@ -157,7 +157,7 @@ PlanStage::StageState DeleteStage::work(WorkingSetID* out) {
             // Ensure that the BSONObj underlying the WorkingSetMember is owned because saveState()
             // is allowed to free the memory.
             if (_params.returnDeleted) {
-                member->makeObjOwned();
+                member->makeObjOwnedIfNeeded();
             }
 
             // TODO: Do we want to buffer docs and delete them in a group rather than
@@ -175,6 +175,9 @@ PlanStage::StageState DeleteStage::work(WorkingSetID* out) {
             }
 
             if (_params.returnDeleted) {
+                // Save a copy of the document that is about to get deleted.
+                BSONObj deletedDoc = member->obj.value();
+                member->obj.setValue(deletedDoc.getOwned());
                 member->loc = RecordId();
                 member->transitionToOwnedObj();
             }
@@ -190,7 +193,7 @@ PlanStage::StageState DeleteStage::work(WorkingSetID* out) {
         } catch (const WriteConflictException& wce) {
             // Ensure that the BSONObj underlying the WorkingSetMember is owned because it may be
             // freed when we yield.
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
             _idRetrying = id;
             memberFreer.Dismiss();  // Keep this member around so we can retry deleting it.
             *out = WorkingSet::INVALID_ID;

src/mongo/db/exec/fetch.cpp
@@ -130,7 +130,7 @@ PlanStage::StageState FetchStage::work(WorkingSetID* out) {
             } catch (const WriteConflictException& wce) {
                 // Ensure that the BSONObj underlying the WorkingSetMember is owned because it may
                 // be freed when we yield.
-                member->makeObjOwned();
+                member->makeObjOwnedIfNeeded();
                 _idRetrying = id;
                 *out = WorkingSet::INVALID_ID;
                 _commonStats.needYield++;

src/mongo/db/exec/merge_sort.cpp
@@ -123,7 +123,7 @@ PlanStage::StageState MergeSortStage::work(WorkingSetID* out) {
             value.id = id;
             value.stage = child;
             // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
             _mergingData.push_front(value);
 
             // Insert the result (indirectly) into our priority queue.

src/mongo/db/exec/multi_plan.cpp
@@ -382,7 +382,7 @@ bool MultiPlanStage::workAllPlans(size_t numResults, PlanYieldPolicy* yieldPolic
             WorkingSetMember* member = candidate.ws->get(id);
             // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we choose to
             // return the results from the 'candidate' plan.
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
             candidate.results.push_back(id);
 
             // Once a plan returns enough results, stop working.

src/mongo/db/exec/near.cpp
@@ -217,7 +217,7 @@ PlanStage::StageState NearStage::bufferNext(WorkingSetID* toReturn, Status* erro
     double memberDistance = distanceStatus.getValue();
 
     // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
-    nextMember->makeObjOwned();
+    nextMember->makeObjOwnedIfNeeded();
     _resultBuffer.push(SearchResult(nextMemberID, memberDistance));
 
     // Store the member's RecordId, if available, for quick invalidation

src/mongo/db/exec/sort.cpp
@@ -274,12 +274,12 @@ void SortStage::addToBuffer(const SortableDataItem& item) {
     WorkingSetMember* member = _ws->get(item.wsid);
     if (_limit == 0) {
         // Ensure that the BSONObj underlying the WorkingSetMember is owned in case we yield.
-        member->makeObjOwned();
+        member->makeObjOwnedIfNeeded();
         _data.push_back(item);
         _memUsage += member->getMemUsage();
     } else if (_limit == 1) {
         if (_data.empty()) {
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
             _data.push_back(item);
             _memUsage = member->getMemUsage();
             return;
@@ -289,7 +289,7 @@ void SortStage::addToBuffer(const SortableDataItem& item) {
         // Compare new item with existing item in vector.
         if (cmp(item, _data[0])) {
             wsidToFree = _data[0].wsid;
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
             _data[0] = item;
             _memUsage = member->getMemUsage();
         }
@@ -298,7 +298,7 @@ void SortStage::addToBuffer(const SortableDataItem& item) {
         // Limit not reached - insert and return
         vector<SortableDataItem>::size_type limit(_limit);
         if (_dataSet->size() < limit) {
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
             _dataSet->insert(item);
             _memUsage += member->getMemUsage();
             return;
@@ -319,7 +319,7 @@ void SortStage::addToBuffer(const SortableDataItem& item) {
             // Here, we choose to erase first to release potential resources
             // used by the last item and to keep the scope of the iterator to a minimum.
             _dataSet->erase(lastItemIt);
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
             _dataSet->insert(item);
         }
     }

src/mongo/db/exec/text_or.cpp
@@ -334,7 +334,7 @@ private:
         WorkingSetMember* member = _ws->get(_id);
 
         // Make it owned since we are buffering results.
-        member->makeObjOwned();
+        member->makeObjOwnedIfNeeded();
         return member->obj.value();
     }
 
@@ -375,7 +375,7 @@ PlanStage::StageState TextOrStage::addTerm(WorkingSetID wsid, WorkingSetID* out)
             } catch (const WriteConflictException& wce) {
                 // Ensure that the BSONObj underlying the WorkingSetMember is owned because it may
                 // be freed when we yield.
-                wsm->makeObjOwned();
+                wsm->makeObjOwnedIfNeeded();
                 _idRetrying = wsid;
                 *out = WorkingSet::INVALID_ID;
                 return NEED_YIELD;

src/mongo/db/exec/update.cpp
@@ -855,7 +855,7 @@ PlanStage::StageState UpdateStage::work(WorkingSetID* out) {
 
             // Ensure that the BSONObj underlying the WorkingSetMember is owned because saveState()
             // is allowed to free the memory.
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
 
             // Save state before making changes
             try {
@@ -869,6 +869,12 @@ PlanStage::StageState UpdateStage::work(WorkingSetID* out) {
                 std::terminate();
             }
 
+            // If we care about the pre-updated version of the doc, save it out here.
+            BSONObj oldObj;
+            if (_params.request->shouldReturnOldDocs()) {
+                oldObj = member->obj.value().getOwned();
+            }
+
             // Do the update, get us the new version of the doc.
             BSONObj newObj = transformAndUpdate(member->obj, loc);
 
@@ -877,6 +883,9 @@ PlanStage::StageState UpdateStage::work(WorkingSetID* out) {
                 if (_params.request->shouldReturnNewDocs()) {
                     member->obj = Snapshotted<BSONObj>(getOpCtx()->recoveryUnit()->getSnapshotId(),
                                                        newObj.getOwned());
+                } else {
+                    invariant(_params.request->shouldReturnOldDocs());
+                    member->obj.setValue(oldObj);
                 }
                 member->loc = RecordId();
                 member->transitionToOwnedObj();
@@ -884,7 +893,7 @@ PlanStage::StageState UpdateStage::work(WorkingSetID* out) {
         } catch (const WriteConflictException& wce) {
             // Ensure that the BSONObj underlying the WorkingSetMember is owned because it may be
             // freed when we yield.
-            member->makeObjOwned();
+            member->makeObjOwnedIfNeeded();
             _idRetrying = id;
             memberFreer.Dismiss();  // Keep this member around so we can retry updating it.
             *out = WorkingSet::INVALID_ID;

src/mongo/db/exec/working_set.cpp
@@ -29,6 +29,7 @@
 #include "mongo/db/exec/working_set.h"
 
 #include "mongo/db/index/index_descriptor.h"
+#include "mongo/db/service_context.h"
 #include "mongo/db/storage/record_fetcher.h"
 
 namespace mongo {
@@ -168,8 +169,8 @@ bool WorkingSetMember::hasOwnedObj() const {
     return _state == OWNED_OBJ || (_state == LOC_AND_OBJ && obj.value().isOwned());
 }
 
-void WorkingSetMember::makeObjOwned() {
-    if (_state == LOC_AND_OBJ && !obj.value().isOwned()) {
+void WorkingSetMember::makeObjOwnedIfNeeded() {
+    if (supportsDocLocking() && _state == LOC_AND_OBJ && !obj.value().isOwned()) {
         obj.setValue(obj.value().getOwned());
     }
 }

src/mongo/db/exec/working_set.h
@@ -293,8 +293,10 @@ public:
     /**
      * Ensures that 'obj' of a WSM in the LOC_AND_OBJ state is owned BSON. It is a no-op if the WSM
      * is in a different state or if 'obj' is already owned.
+     *
+     * It is also a no-op if the active storage engine doesn't support document-level concurrency.
      */
-    void makeObjOwned();
+    void makeObjOwnedIfNeeded();
 
     //
     // Computed data

src/mongo/dbtests/query_stage_sort.cpp
@@ -170,7 +170,7 @@ public:
         // totally) correct.
         BSONObj last;
         ASSERT_EQUALS(PlanExecutor::ADVANCED, exec->getNext(&last, NULL));
-        ASSERT(last.isOwned());
+        last = last.getOwned();
 
         // Count 'last'.
         int count = 1;
@@ -182,8 +182,7 @@ public:
             // pattern.
             ASSERT(cmp == 0 || cmp == 1);
             ++count;
-            last = current;
-            ASSERT(last.isOwned());
+            last = current.getOwned();
         }
 
         checkCount(count);
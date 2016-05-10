MitsukiSUGITA/my_mongoDB

commit 9b46bab6d0edc7e2f995922f474d45127870710f

SERVER-16203 Add command to display global lock information

src/mongo/db/commands/lock_info.cpp
@@ -1,0 +1,101 @@
+/**
+ * Copyright (C) 2016 MongoDB Inc.
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * As a special exception, the copyright holders give permission to link the
+ * code of portions of this program with the OpenSSL library under certain
+ * conditions as described in each individual source file and distribute
+ * linked combinations including the program with the OpenSSL library. You
+ * must comply with the GNU Affero General Public License in all respects
+ * for all of the code used other than as permitted herein. If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version. If you
+ * delete this exception statement from all source files in the program,
+ * then also delete it in the license file.
+ */
+
+#include <map>
+
+#include "mongo/db/client.h"
+#include "mongo/db/commands.h"
+#include "mongo/db/concurrency/lock_manager_defs.h"
+#include "mongo/db/concurrency/lock_state.h"
+#include "mongo/db/operation_context.h"
+#include "mongo/db/service_context.h"
+
+namespace mongo {
+
+using std::string;
+using std::stringstream;
+
+/**
+ * Admin command to display global lock information
+ */
+class CmdLockInfo : public Command {
+public:
+    virtual bool slaveOk() const {
+        return true;
+    }
+
+    virtual bool slaveOverrideOk() const {
+        return true;
+    }
+
+    virtual bool adminOnly() const {
+        return true;
+    }
+
+    virtual bool supportsWriteConcern(const BSONObj& cmd) const {
+        return false;
+    }
+
+    virtual void help(stringstream& help) const {
+        help << "show all lock info on the server";
+    }
+
+    CmdLockInfo() : Command("lockInfo", true) {}
+
+    bool run(OperationContext* txn,
+             const string& dbname,
+             BSONObj& jsobj,
+             int,
+             string& errmsg,
+             BSONObjBuilder& result) {
+        std::map<LockerId, BSONObj> lockToClientMap;
+
+        for (ServiceContext::LockedClientsCursor cursor(txn->getClient()->getServiceContext());
+             Client* client = cursor.next();) {
+            invariant(client);
+
+            stdx::lock_guard<Client> lk(*client);
+            const OperationContext* opCtx = client->getOperationContext();
+
+            // Operation context specific information
+            if (opCtx) {
+                BSONObjBuilder infoBuilder;
+                // The client information
+                client->reportState(infoBuilder);
+
+                infoBuilder.append("opid", opCtx->getOpID());
+                LockerId lockerId = opCtx->lockState()->getId();
+                lockToClientMap.insert({lockerId, infoBuilder.obj()});
+            }
+        }
+
+        getGlobalLockManager()->getLockInfoBSON(lockToClientMap, &result);
+        return true;
+    }
+} cmdLockInfo;
+}

src/mongo/db/SConscript
@@ -602,6 +602,7 @@ serverOnlyFiles = [
     "commands/list_collections.cpp",
     "commands/list_databases.cpp",
     "commands/list_indexes.cpp",
+    "commands/lock_info.cpp",
     "commands/mr.cpp",
     "commands/oplog_note.cpp",
     "commands/parallel_collection_scan.cpp",

src/mongo/db/concurrency/lock_manager.cpp
@@ -30,10 +30,9 @@
 
 #include "mongo/platform/basic.h"
 
-#include <vector>
-
 #include "mongo/db/concurrency/lock_manager.h"
 
+#include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/config.h"
 #include "mongo/db/concurrency/locker.h"
 #include "mongo/util/assert_util.h"
@@ -661,33 +660,36 @@ void LockManager::downgrade(LockRequest* request, LockMode newMode) {
 }
 
 void LockManager::cleanupUnusedLocks() {
-    size_t deletedLockHeads = 0;
     for (unsigned i = 0; i < _numLockBuckets; i++) {
         LockBucket* bucket = &_lockBuckets[i];
         stdx::lock_guard<SimpleMutex> scopedLock(bucket->mutex);
+        _cleanupUnusedLocksInBucket(bucket);
+    }
+}
 
-        LockBucket::Map::iterator it = bucket->data.begin();
-        while (it != bucket->data.end()) {
-            LockHead* lock = it->second;
-            if (lock->partitioned()) {
-                lock->migratePartitionedLockHeads();
-            }
-            if (lock->grantedModes == 0) {
-                invariant(lock->grantedModes == 0);
-                invariant(lock->grantedList._front == NULL);
-                invariant(lock->grantedList._back == NULL);
-                invariant(lock->conflictModes == 0);
-                invariant(lock->conflictList._front == NULL);
-                invariant(lock->conflictList._back == NULL);
-                invariant(lock->conversionsCount == 0);
-                invariant(lock->compatibleFirstCount == 0);
+void LockManager::_cleanupUnusedLocksInBucket(LockBucket* bucket) {
+    LockBucket::Map::iterator it = bucket->data.begin();
+    size_t deletedLockHeads = 0;
+    while (it != bucket->data.end()) {
+        LockHead* lock = it->second;
+        if (lock->partitioned()) {
+            lock->migratePartitionedLockHeads();
+        }
+        if (lock->grantedModes == 0) {
+            invariant(lock->grantedModes == 0);
+            invariant(lock->grantedList._front == NULL);
+            invariant(lock->grantedList._back == NULL);
+            invariant(lock->conflictModes == 0);
+            invariant(lock->conflictList._front == NULL);
+            invariant(lock->conflictList._back == NULL);
+            invariant(lock->conversionsCount == 0);
+            invariant(lock->compatibleFirstCount == 0);
 
-                bucket->data.erase(it++);
-                deletedLockHeads++;
-                delete lock;
-            } else {
-                it++;
-            }
+            bucket->data.erase(it++);
+            deletedLockHeads++;
+            delete lock;
+        } else {
+            it++;
         }
     }
 }
@@ -820,6 +822,70 @@ void LockManager::dump() const {
     }
 }
 
+void LockManager::_dumpBucketToBSON(const std::map<LockerId, BSONObj>& lockToClientMap,
+                                    const LockBucket* bucket,
+                                    BSONObjBuilder* result) {
+    for (auto& bucketEntry : bucket->data) {
+        const LockHead* lock = bucketEntry.second;
+
+        if (lock->grantedList.empty()) {
+            // If there are no granted requests, this lock is empty, so no need to print it
+            continue;
+        }
+
+        result->append("resourceId", lock->resourceId.toString());
+
+        BSONArrayBuilder grantedLocks;
+        for (const LockRequest* iter = lock->grantedList._front; iter != nullptr;
+             iter = iter->next) {
+            _buildBucketBSON(iter, lockToClientMap, bucket, &grantedLocks);
+        }
+        result->append("granted", grantedLocks.arr());
+
+        BSONArrayBuilder pendingLocks;
+        for (const LockRequest* iter = lock->conflictList._front; iter != nullptr;
+             iter = iter->next) {
+            _buildBucketBSON(iter, lockToClientMap, bucket, &pendingLocks);
+        }
+        result->append("pending", pendingLocks.arr());
+    }
+}
+
+void LockManager::_buildBucketBSON(const LockRequest* iter,
+                                   const std::map<LockerId, BSONObj>& lockToClientMap,
+                                   const LockBucket* bucket,
+                                   BSONArrayBuilder* locks) {
+    BSONObjBuilder info;
+    info.append("mode", modeName(iter->mode));
+    info.append("convertMode", modeName(iter->convertMode));
+    info.append("enqueueAtFront", iter->enqueueAtFront);
+    info.append("compatibleFirst", iter->compatibleFirst);
+
+    LockerId lockerId = iter->locker->getId();
+    std::map<LockerId, BSONObj>::const_iterator it = lockToClientMap.find(lockerId);
+    if (it != lockToClientMap.end()) {
+        info.appendElements(it->second);
+    }
+    locks->append(info.obj());
+}
+
+void LockManager::getLockInfoBSON(const std::map<LockerId, BSONObj>& lockToClientMap,
+                                  BSONObjBuilder* result) {
+    BSONArrayBuilder lockInfo;
+    for (unsigned i = 0; i < _numLockBuckets; i++) {
+        LockBucket* bucket = &_lockBuckets[i];
+        stdx::lock_guard<SimpleMutex> scopedLock(bucket->mutex);
+
+        _cleanupUnusedLocksInBucket(bucket);
+        if (!bucket->data.empty()) {
+            BSONObjBuilder b;
+            _dumpBucketToBSON(lockToClientMap, bucket, &b);
+            lockInfo.append(b.obj());
+        }
+    }
+    result->append("lockInfo", lockInfo.arr());
+}
+
 void LockManager::_dumpBucket(const LockBucket* bucket) const {
     for (LockBucket::Map::const_iterator it = bucket->data.begin(); it != bucket->data.end();
          it++) {
@@ -852,7 +918,6 @@ void LockManager::_dumpBucket(const LockBucket* bucket) const {
         }
 
         sb << "-----------------------------------------------------------\n";
-
         log() << sb.str();
     }
 }

src/mongo/db/concurrency/lock_manager.h
@@ -30,7 +30,10 @@
 
 #include <cstdint>
 #include <deque>
+#include <map>
+#include <vector>
 
+#include "mongo/bson/bsonobj.h"
 #include "mongo/config.h"
 #include "mongo/db/concurrency/lock_manager_defs.h"
 #include "mongo/db/concurrency/lock_request_list.h"
@@ -121,6 +124,13 @@ public:
      */
     void dump() const;
 
+    /**
+     * Dumps the contents of all locks into a BSON object
+     * to be used in lockInfo command in the shell.
+     */
+    void getLockInfoBSON(const std::map<LockerId, BSONObj>& lockToClientMap,
+                         BSONObjBuilder* result);
+
 private:
     // The deadlock detector needs to access the buckets and locks directly
     friend class DeadlockDetector;
@@ -166,6 +176,23 @@ private:
     void _dumpBucket(const LockBucket* bucket) const;
 
     /**
+     * Dump the contents of a bucket to the BSON.
+     */
+    void _dumpBucketToBSON(const std::map<LockerId, BSONObj>& lockToClientMap,
+                           const LockBucket* bucket,
+                           BSONObjBuilder* result);
+
+    /**
+     * Build the BSON object containing the lock info for a particular
+     * bucket. The lockToClientMap is used to map the lockerId to
+     * more useful client information.
+     */
+    void _buildBucketBSON(const LockRequest* iter,
+                          const std::map<LockerId, BSONObj>& lockToClientMap,
+                          const LockBucket* bucket,
+                          BSONArrayBuilder* locks);
+
+    /**
      * Should be invoked when the state of a lock changes in a way, which could potentially
      * allow other blocked requests to proceed.
      *
@@ -177,6 +204,12 @@ private:
      *          granted modes, which was conflicting before became zero.
      */
     void _onLockModeChanged(LockHead* lock, bool checkConflictQueue);
+
+    /**
+     * Helper function to delete all locks that have no request on them on a single bucket.
+     * Called by cleanupUnusedLocks()
+     */
+    void _cleanupUnusedLocksInBucket(LockBucket* bucket);
 
     static const unsigned _numLockBuckets;
     LockBucket* _lockBuckets;

commit 2eb82dc6642a4db2d63f3612f6ad1248bbdf9bf3

SERVER-16203 lock_info.cpp must include basic.h
Fixes compile failure on Windows introduced by 9b46bab6.

src/mongo/db/commands/lock_info.cpp
@@ -26,6 +26,8 @@
  * then also delete it in the license file.
  */
 
+#include "mongo/platform/basic.h"
+
 #include <map>
 
 #include "mongo/db/client.h"
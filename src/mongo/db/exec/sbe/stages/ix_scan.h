dstorch/mongo

SERVER-50710 Consolidate SBE and classic yielding paths
SBE will now, like the classic engine, use
saveLockStateAndUnlock() in order to yield any locks held
higher on the stack.  Additionally, this patch enables the
'concurrency' suite in the SBE build variant in order to
provide additional yielding-related test coverage.

src/mongo/db/exec/sbe/stages/ix_scan.h
@@ -31,7 +31,7 @@
 
 #include "mongo/bson/ordering.h"
 #include "mongo/db/db_raii.h"
-#include "mongo/db/exec/sbe/stages/lock_acquisition_callback.h"
+#include "mongo/db/exec/sbe/stages/restore_collection.h"
 #include "mongo/db/exec/sbe/stages/stages.h"
 #include "mongo/db/storage/record_store.h"
 #include "mongo/db/storage/sorted_data_interface.h"
@@ -59,7 +59,7 @@ namespace mongo::sbe {
  */
 class IndexScanStage final : public PlanStage {
 public:
-    IndexScanStage(const NamespaceStringOrUUID& name,
+    IndexScanStage(CollectionUUID collUuid,
                    std::string_view indexName,
                    bool forward,
                    boost::optional<value::SlotId> recordSlot,
@@ -93,7 +93,7 @@ protected:
     void doAttachToTrialRunTracker(TrialRunTracker* tracker) override;
 
 private:
-    const NamespaceStringOrUUID _name;
+    const CollectionUUID _collUuid;
     const std::string _indexName;
     const bool _forward;
     const boost::optional<value::SlotId> _recordSlot;
@@ -102,6 +102,8 @@ private:
     const value::SlotVector _vars;
     const boost::optional<value::SlotId> _seekKeySlotLow;
     const boost::optional<value::SlotId> _seekKeySlotHigh;
+
+    NamespaceString _collName;
 
     LockAcquisitionCallback _lockAcquisitionCallback;
 

src/mongo/db/query/sbe_stage_builder.h
@@ -30,7 +30,7 @@
 #pragma once
 
 #include "mongo/db/exec/sbe/expressions/expression.h"
-#include "mongo/db/exec/sbe/stages/lock_acquisition_callback.h"
+#include "mongo/db/exec/sbe/stages/restore_collection.h"
 #include "mongo/db/exec/sbe/values/slot.h"
 #include "mongo/db/exec/sbe/values/value.h"
 #include "mongo/db/exec/trial_period_utils.h"

src/mongo/db/query/sbe_stage_builder_coll_scan.h
@@ -30,7 +30,7 @@
 #pragma once
 
 #include "mongo/db/exec/sbe/expressions/expression.h"
-#include "mongo/db/exec/sbe/stages/lock_acquisition_callback.h"
+#include "mongo/db/exec/sbe/stages/restore_collection.h"
 #include "mongo/db/exec/sbe/stages/stages.h"
 #include "mongo/db/exec/sbe/values/value.h"
 #include "mongo/db/query/query_solution.h"

MitsukiSUGITA/my_mongoDB

src/mongo/db/exec/sbe/stages/ix_scan.h
@@ -31,7 +31,7 @@
 
 #include "mongo/bson/ordering.h"
 #include "mongo/db/db_raii.h"
-#include "mongo/db/exec/sbe/stages/lock_acquisition_callback.h"
+#include "mongo/db/exec/sbe/stages/collection_helpers.h"
 #include "mongo/db/exec/sbe/stages/stages.h"
 #include "mongo/db/storage/record_store.h"
 #include "mongo/db/storage/sorted_data_interface.h"
@@ -59,7 +59,7 @@ namespace mongo::sbe {
  */
 class IndexScanStage final : public PlanStage {
 public:
-    IndexScanStage(const NamespaceStringOrUUID& name,
+    IndexScanStage(CollectionUUID collUuid,
                    std::string_view indexName,
                    bool forward,
                    boost::optional<value::SlotId> recordSlot,
@@ -93,7 +93,7 @@ protected:
     void doAttachToTrialRunTracker(TrialRunTracker* tracker) override;
 
 private:
-    const NamespaceStringOrUUID _name;
+    const CollectionUUID _collUuid;
     const std::string _indexName;
     const bool _forward;
     const boost::optional<value::SlotId> _recordSlot;
@@ -102,6 +102,8 @@ private:
     const value::SlotVector _vars;
     const boost::optional<value::SlotId> _seekKeySlotLow;
     const boost::optional<value::SlotId> _seekKeySlotHigh;
+
+    NamespaceString _collName;
 
     LockAcquisitionCallback _lockAcquisitionCallback;
 

src/mongo/db/query/sbe_stage_builder.h
@@ -30,7 +30,7 @@
 #pragma once
 
 #include "mongo/db/exec/sbe/expressions/expression.h"
-#include "mongo/db/exec/sbe/stages/lock_acquisition_callback.h"
+#include "mongo/db/exec/sbe/stages/collection_helpers.h"
 #include "mongo/db/exec/sbe/values/slot.h"
 #include "mongo/db/exec/sbe/values/value.h"
 #include "mongo/db/exec/trial_period_utils.h"

src/mongo/db/query/sbe_stage_builder_coll_scan.h
@@ -30,7 +30,7 @@
 #pragma once
 
 #include "mongo/db/exec/sbe/expressions/expression.h"
-#include "mongo/db/exec/sbe/stages/lock_acquisition_callback.h"
+#include "mongo/db/exec/sbe/stages/collection_helpers.h"
 #include "mongo/db/exec/sbe/stages/stages.h"
 #include "mongo/db/exec/sbe/values/value.h"
 #include "mongo/db/query/query_solution.h"